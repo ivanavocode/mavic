56e5640ea8122ac78453e3f53a97928e7a06fe44
ivanavocode <47441854+ivanavocode@users.noreply.github.com>
2020-11-10 11:03:41 +0600

    commit

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,10 @@ $(function(){
         $(this).parent().addClass('questions__item--active');
     })
 
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true,
+        sectionSelector: '.page-section',
+    })
+
 });
